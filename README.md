0c0669ff049a875f0fbc7a732fb3248f5b2edf78
Jack Gilmore <46202639+JackGilmore@users.noreply.github.com>
2024-06-11 19:20:02 +0000

    #1 Initial script to extract dataset from PDF

--- README.md
+++ README.md
@@ -1,1 +1,37 @@
 # PrisonerDataAnalysis
+
+## About the project
+
+A Python application that processes prisoner data, provides a REST API for accessing the data, and displays the data as interactive charts in a web browser.
+
+### Built with
+- Python 3.10
+    - PyMuPDF
+
+## Getting started
+
+To get a local copy up and running, follow these steps:
+
+### Prerequisites
+
+You should have the following installed on your machine:
+- [Python 3](https://www.python.org/) (3.10 was used during development)
+- [pip](https://pypi.org/project/pip/)
+
+If you are using GitHub codespaces, these should be available by default.
+
+Make sure you run `pip install -r requirements.txt` first from the `src` folder.
+
+## load_data script usage
+
+This script will load and manipulate the dataset using Pandas, outputting it as an SQLite file
+
+```shell
+
+python .\load_data.py
+
+```
+<!-- 
+## API usage
+
+TODO -->
---
+++ src/load_data.py
@@ -0,0 +1,118 @@
+#!/usr/bin/env python3
+
+"""
+Script Name: load_data.py
+Description: This script loads and manipulates the prisoner dataset using Pandas.
+Author: Jack Gilmore
+Date: 2024-06-11
+"""
+
+import os
+import sys
+import logging
+import pymupdf
+from typing import List
+
+# Configure logging
+logging.basicConfig(
+    level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s"
+)
+
+# Constants
+DATA_SOURCE_NAME = "coding-test.pdf"
+DATASET_HEADER = "prisoner_id,name,age,gender,crime,sentence_years,prison"
+
+
+def load_data() -> list:
+    """
+    Loads the data from the PDF, extracts the text and truncates the text lines down to the relevant dataset
+
+    Returns:
+    list: A string array of the dataset with each item a comma separated string for a row in the dataset
+
+    """
+    logging.info(f"Opening {DATA_SOURCE_NAME}")
+
+    # Build the base path using the folder our script lives in
+    script_path = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
+    data_source_path = os.path.join(script_path, DATA_SOURCE_NAME)
+
+    # Open the document
+    data_source_document = None
+    try:
+        data_source_document = pymupdf.open(data_source_path)
+    except pymupdf.FileNotFoundError:
+        logging.error(f"Could not find file at path {data_source_path}")
+    except Exception as e:
+        logging.error(f"An unexpected error occurred: {e}")
+
+    # If we couldn't read the file for some reason, error out gracefully
+    if data_source_document == None:
+        sys.exit(1)
+
+    data_source_lines = []
+
+    logging.info(f"Extracting text from {DATA_SOURCE_NAME}")
+
+    # Loop through the pages, skipping the first one as it contains no relevant content to scrape
+    for page in data_source_document[1:]:
+        # Get plain text encoded as UTF-8
+        page_text = page.get_text()
+
+        # Split text by newlines and strip whitespace
+        page_lines = [line.strip() for line in str.splitlines(page_text)]
+
+        # Add the lines to our full list of document lines
+        data_source_lines += page_lines
+
+    line_count = len(data_source_lines)
+
+    logging.info(f"Line count: {line_count}")
+
+    logging.info("Finding start of dataset")
+
+    # Find the text with the header for the CSV
+    # We want to ignore any text before this as it isn't data. Just instructions.
+    header_index = next(
+        (
+            index
+            for index, line_string in enumerate(data_source_lines)
+            if DATASET_HEADER in line_string
+        ),
+        -1,
+    )
+
+    # If we don't get a header back, error out gracefully
+    if header_index == -1:
+        logging.error(f"Could not find a header row with value of {DATASET_HEADER}")
+        sys.exit(1)
+
+    logging.info(f"Header index found at {header_index}")
+
+    # Truncate our PDF lines down to just the ones from the header onwards
+    data_source = data_source_lines[header_index:]
+
+    data_source_count = len(data_source)
+
+    logging.info(
+        f"Successfully retrieved {data_source_count} rows for dataset (including header)"
+    )
+
+    return data_source
+
+
+def main(args: List[str]) -> None:
+    """
+    Main function that orchestrates the script's functionality.
+
+    Parameters:
+    args: A list of arguments
+    """
+    logging.info("Starting processing...")
+
+    # Extract from PDF and load as an array of comma separated strings
+    raw_dataset = load_data()
+
+
+if __name__ == "__main__":
+    main(sys.argv)
